d1b6038cd346075de8bece2b4e3f0c006c178114
Dvirflex <115162343+Dvirflex@users.noreply.github.com>
2022-10-06 21:00:27 +0300

    DVIR

---
+++ fix age.py
@@ -0,0 +1,15 @@
+def greet(person, first_time=False):
+    if first_time:
+        return "First time for everything, right? Welcome, " + person
+    return "Hello, " + person
+
+
+def greet_all(people):
+    for person in people:
+        print(greet(person, True))
+
+
+friends = ["bob", "Josh", "Austin"]
+
+
+greet_all(friends)
